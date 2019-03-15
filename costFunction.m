d9c10b1359ac488ff761be0ab8df87c48085e021
SriRamLakshmiNarasimhan <sriramnarasimhan1990@gmail.com>
2019-03-15 21:29:39 +0530

    modified file for CRLF

--- costFunction.m
+++ costFunction.m
@@ -1,2 +1,14 @@
-function [cost costHistory theta] = costFunction(X, y, theta, alpha, iterations)    costHistory = zeros(iterations, 1);    for i=1:iterations    m = size(X,1);    pred = sigmoid(X, theta);     gradDescend = X'*(pred - y);      theta = theta - (alpha/m)*gradDescend;    cost = (-1/m)*sum((y.*log(pred) + (1-y).*log(1-pred))(:));    costHistory(i) = cost;  endfor    
+function [cost costHistory theta] = costFunction(X, y, theta, alpha, iterations)
+  
+  costHistory = zeros(iterations, 1);
+  
+  for i=1:iterations
+    m = size(X,1);
+    pred = sigmoid(X, theta); 
+    gradDescend = X'*(pred - y);  
+    theta = theta - (alpha/m)*gradDescend;
+    cost = (-1/m)*sum((y.*log(pred) + (1-y).*log(1-pred))(:));
+    costHistory(i) = cost;
+  endfor  
+  
 endfunction
